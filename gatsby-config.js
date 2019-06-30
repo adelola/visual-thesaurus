e679388c418ad219b8e5f16d9776515a85cd93b3
adelola <lola.adekunle@gmail.com>
2019-06-29 23:23:10 -0400

    Initialize gatsby starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Visual Thesaurus`,
+    description: `A visual (optical ocular perceptible imaged viewable) way to get synonyms`,
+    author: `Adelola`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `visual-thesaurus`,
+        short_name: `vhesaurus`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FF3F00`,
+        theme_color: `#FF7F11`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
